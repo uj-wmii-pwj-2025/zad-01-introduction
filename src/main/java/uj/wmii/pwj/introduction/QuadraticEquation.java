5ac99a7b8fe30de9cb1a74a195eaf4461fe7bfa3
kingazm <kinga.zmuda@icloud.com>
2025-10-04 13:51:11 +0200

    Add task 01 solutions

--- src/main/java/uj/wmii/pwj/introduction/QuadraticEquation.java
+++ src/main/java/uj/wmii/pwj/introduction/QuadraticEquation.java
@@ -1,9 +1,31 @@
 package uj.wmii.pwj.introduction;
 
-public class QuadraticEquation {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+public class QuadraticEquation {
+    private static final Logger log = LoggerFactory.getLogger(QuadraticEquation.class);
     public double[] findRoots(double a, double b, double c) {
-        return null;
+        /* Infinite number of solutions case - not specified in the task
+        Handling this case depends on whether we restrict values of a,b,c in any way
+         */
+        if (a == 0 && b == 0 && c == 0) {
+            log.info("Provided equation is 0 = 0, therefore has infinite number of solutions.");
+            return null; //Handled differently than typical "No solutions case", as 0 = 0 is true for every x
+        }
+
+        double delta = b * b - 4.0 * a * c;
+
+        if (delta == 0.0) { //One solution
+            double x = -b / (2 * a);
+            return new double[]{x};
+        } else if (delta > 0.0) { //Two solutions
+            double deltaSqrt = Math.sqrt(delta);
+            double x1 = (-b - deltaSqrt) / (2 * a);
+            double x2 = (-b + deltaSqrt) / (2 * a);
+            return new double[]{x1, x2};
+        }
+        return new double[0]; //No solutions
     }
 
 }
