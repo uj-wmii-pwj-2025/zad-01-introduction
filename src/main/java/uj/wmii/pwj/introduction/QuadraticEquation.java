661bb334ec865cf539f0d4dcaff96e16a2752f2b
Mikolaj Mroczek <mikolaj.mroczek.dev@gmail.com>
2025-10-09 17:01:54 +0200

    Added solutions

--- src/main/java/uj/wmii/pwj/introduction/QuadraticEquation.java
+++ src/main/java/uj/wmii/pwj/introduction/QuadraticEquation.java
@@ -1,9 +1,25 @@
 package uj.wmii.pwj.introduction;
 
+
+
 public class QuadraticEquation {
 
     public double[] findRoots(double a, double b, double c) {
-        return null;
+        if(Math.abs(a) < 1e-12)
+        {
+            if(Math.abs(b) < 1e-12)
+            {
+                throw new IllegalArgumentException("Invalid equation");
+            }
+          return new double[] {c / b};  
+        } 
+
+        double delta = Math.pow(a , 2) - 4 * a * c;
+
+        double x1 = (-b - Math.sqrt(delta) ) / 4 * a;
+        double x2 = (-b + Math.sqrt(delta) ) / 4 * a;
+
+        return new double[]{x1, x2};
     }
 
 }
